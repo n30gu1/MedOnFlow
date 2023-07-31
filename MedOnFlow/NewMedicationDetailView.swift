165922a0d05d3aa5d40cec38900f56da9001de2a
n30gu1 <n@n30gu1.com>
2023-08-01 07:23:30 +0900

    Add NewMedicationDeatilView

---
+++ MedOnFlow/NewMedicationDetailView.swift
@@ -0,0 +1,19 @@
+//
+//  NewMedicationDetailView.swift
+//  MedOnFlow
+//
+//  Created by Sung Park on 8/1/23.
+//
+
+import SwiftUI
+
+struct NewMedicationDetailView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewMedicationDetailView()
+}
